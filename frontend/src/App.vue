41f838de68568b33a1f242c744ce5611d97546af
cameronbaffuto <cam@cambaffuto.com>
2025-12-26 18:38:53 -0500

    init

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex flex-col h-screen">
+    <main class="flex-1 overflow-auto">
+      <router-view />
+    </main>
+  </div>
+</template>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center h-full">
+    <h1 class="text-4xl text-green-900">Welcome to HomeBones!</h1>
+    <p class="mt-4 text-amber-500">Your smart home management system</p>
+  </div>
+</template>
